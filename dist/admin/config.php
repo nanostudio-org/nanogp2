643cb5fd274af715625f9330cbea3b50f9da82ec
Christophe Brisbois <chr@brisbois.fr>
2019-02-03 15:41:47 +0100

    Add files via upload

---
+++ dist/admin/config.php
@@ -0,0 +1,7 @@
+<?php
+$cfg_client_id          = '1048718721095-89ae57i124b8f6ouugkbaimek2nkb83h.apps.googleusercontent.com';
+$cfg_client_secret      = '9zfnClycWVOM2IsO-B2RaIrp';
+$cfg_max_accounts       = 1;
+$cfg_log                = false;
+$albums_filter          = ['sauvegarde', 'backup'];
+?>
