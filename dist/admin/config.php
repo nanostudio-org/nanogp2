2694f9eb950375611a601b79fd22725c8e160ec3
Christophe Brisbois <chr@brisbois.fr>
2019-03-07 23:48:43 +0100

    Update config.php

--- dist/admin/config.php
+++ dist/admin/config.php
@@ -1,6 +1,6 @@
 <?php
-$cfg_client_id          = '1048718721095-89ae57i124b8f6ouugkbaimek2nkb83h.apps.googleusercontent.com';
-$cfg_client_secret      = '9zfnClycWVOM2IsO-B2RaIrp';
+$cfg_client_id          = 'your_client_id';
+$cfg_client_secret      = 'your_client_secret';
 $cfg_max_accounts       = 1;
 $cfg_log                = false;
 $albums_filter          = ['sauvegarde', 'backup'];
